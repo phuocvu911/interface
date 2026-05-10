25b9fbd55c21926b7bd8879b028b69d292f331a4
Hoang Phuoc Vu <iamphuocvux@gmail.com>
2026-05-10 12:12:33 +0300

    descriptions for the fields in pageData struct

--- cmd/web/model/model.go
+++ cmd/web/model/model.go
@@ -1,10 +1,10 @@
 package model
 
 type pageData struct {
-	Mode       string
-	Input      string
-	Output     string
-	StatusCode int
-	StatusText string
-	Error      string
+	Mode       string // "encode" or "decode"
+	Input      string // user input from the form
+	Output     string // the result of encoding or decoding the input
+	StatusCode int    // HTTP status code to indicate success or type of error
+	StatusText string // human-readable text of StatusCode
+	Error      string // detailed error message for display in the UI if something went wrong
 }
